70ea783fef226d1dfc771fb5cccd43e36047fb3e
unknown <mittalshubham05@gmail.com>
2020-10-08 10:10:59 +0530

    sign for player on tic tac toe board determined

--- src/main/java/bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/main/java/bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,7 +1,11 @@
 package bridgelabz.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	private char[] board;
+	private char playerSign;
+	private char computerSign;
 
 	/**
 	 * creates the initial board for playing
@@ -23,5 +27,26 @@ public class TicTacToeGame {
 		System.out.println("Welcome to Tic Tac Toe game");
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
 		ticTacToeGame.createBoard();
+		ticTacToeGame.determinePlayerCharacter();
+	}
+
+	/**
+	 * this method determines sign for user on tic tac toe board
+	 */
+	private void determinePlayerCharacter() {
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+			System.out.println("Enter X or O for assigning character to Player");
+			String s= sc.nextLine();
+			playerSign = s.charAt(0);
+			if(playerSign!= 'X' && playerSign!='O')
+				continue;
+			break;
+		}
+		if(playerSign == 'X')
+			computerSign = 'O';
+		else
+			computerSign = 'X';
+		System.out.println("The character chosen by player is " + playerSign);
 	}
 }
