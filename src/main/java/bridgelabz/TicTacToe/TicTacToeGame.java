95e0ea9cfc267abda361ed52b7a4ce0b54c7cd76
unknown <mittalshubham05@gmail.com>
2020-10-08 09:35:16 +0530

    Proper formatting provided

--- src/main/java/bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/main/java/bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,27 +1,27 @@
 package bridgelabz.TicTacToe;
 
-public class TicTacToeGame 
-{
-	private char []board;
-	
+public class TicTacToeGame {
+	private char[] board;
+
 	/**
 	 * creates the initial board for playing
 	 */
 	public void createBoard() {
-		this.board =  new char[10];
-		for(int index = 1; index < 10 ; index++ )
+		this.board = new char[10];
+		for (int index = 1; index < 10; index++)
 			board[index] = ' ';
 	}
+
 	/**
 	 * constructor for TicTacToeGame
 	 */
 	public TicTacToeGame() {
 		super();
 	}
-	public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Tic Tac Toe game" );
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        ticTacToeGame.createBoard();
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe game");
+		TicTacToeGame ticTacToeGame = new TicTacToeGame();
+		ticTacToeGame.createBoard();
+	}
 }
